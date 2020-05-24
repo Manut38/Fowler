d643a577999db238e657acfb2b428d8f67874283
Manuel Piroch <manuel@pepit.net>
2020-05-24 20:31:29 +0200

    renamed variable

--- src/Customer.java
+++ src/Customer.java
@@ -24,9 +24,8 @@ class Customer {
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         for (Rental rental : rentals) {
-            double thisAmount = 0;
-            // determine amounts for each line
-            thisAmount = rental.getAmount();
+            // get amount for current rental
+            double amount = rental.getAmount();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -34,8 +33,9 @@ class Customer {
                 frequentRenterPoints++;
             // show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" + "\t" + rental.getDaysRented() + "\t"
-                    + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    + String.valueOf(amount) + "\n";
+            //add current rental amount to total
+            totalAmount += amount;
         }
         // add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
