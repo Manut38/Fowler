cc01d6ceb221c5b714a97a741ad209878467b25f
Manuel Piroch <manuel@pepit.net>
2020-05-24 20:24:38 +0200

    moved function and field

--- src/Rental.java
+++ src/Rental.java
@@ -1,10 +1,32 @@
 class Rental {
     private Movie movie;
     private int daysRented;
+    private double amount;
 
     public Rental(Movie newMovie, int newDaysRented) {
         movie = newMovie;
         daysRented = newDaysRented;
+        amount = calculateAmount();
+    }
+
+    private double calculateAmount() {
+        double amount = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                amount = 2;
+                if (daysRented > 2)
+                    amount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount = daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount = 1.5;
+                if (daysRented > 3)
+                    amount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return amount;
     }
 
     public int getDaysRented() {
@@ -13,5 +35,9 @@ class Rental {
 
     public Movie getMovie() {
         return movie;
+    }
+
+    public double getAmount() {
+        return amount;
     }
 }
