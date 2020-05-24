7bdfdd3dd66c352b74d7fce4c387cfb98391536c
Manuel Piroch <manuel@pepit.net>
2020-05-24 20:00:51 +0200

    changed loop to foreach

--- src/Customer.java
+++ src/Customer.java
@@ -1,5 +1,4 @@
 
-import java.util.Enumeration;
 import java.util.Vector;
 
 class Customer {
@@ -21,22 +20,20 @@ class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
-        while (enum_rentals.hasMoreElements()) {
+        for (Rental rental : rentals) {
             double thisAmount = 0;
-            Rental each = (Rental) enum_rentals.nextElement();
             // determine amounts for each line
-            thisAmount = amountFor(each);
+            thisAmount = amountFor(rental);
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
                 frequentRenterPoints++;
             // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
+            result += "\t" + rental.getMovie().getTitle() + "\t" + "\t" + rental.getDaysRented() + "\t"
                     + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
